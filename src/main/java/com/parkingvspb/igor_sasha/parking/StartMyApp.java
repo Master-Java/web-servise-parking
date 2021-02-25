b6b81b8f41f70d038855802fad6688bf06e419d9
QWERTY <nio07212000@mail.ru>
2021-02-25 18:24:18 +0300

    This is the beginning of the backend development

---
+++ src/main/java/com/parkingvspb/igor_sasha/parking/StartMyApp.java
@@ -0,0 +1,13 @@
+package com.parkingvspb.igor_sasha.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartMyApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartMyApp.class, args);
+    }
+
+}
